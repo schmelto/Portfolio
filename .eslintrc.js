b9d23cb9150c7eca775a35705f47285af492384b
schmelto <30869493+schmelto@users.noreply.github.com>
2021-04-30 17:35:49 +0200

    fix: format style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,13 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {},
 };
