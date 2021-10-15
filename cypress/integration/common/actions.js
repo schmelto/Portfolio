687b38cf68e2593084d2da425b1317e9c05d5e93
schmelto <30869493+schmelto@users.noreply.github.com>
2021-10-15 23:13:26 +0200

    style: fix prettier issues

--- cypress/integration/common/actions.js
+++ cypress/integration/common/actions.js
@@ -1,15 +1,14 @@
 /* eslint-disable no-undef */
-import { Given, Then } from 'cypress-cucumber-preprocessor/steps'
+import { Given, Then } from 'cypress-cucumber-preprocessor/steps';
 
 Given('I open the website', () => {
-  cy.visit('index.html') 
-})
+  cy.visit('index.html');
+});
 
 Then(`I see {string} in the title`, (title) => {
-  cy.title().should("include", title);
+  cy.title().should('include', title);
 });
 
 Then(`I see {string} in {string}`, (text, location) => {
-  cy.get(location)
-  .should('contain', text)
-})
+  cy.get(location).should('contain', text);
+});
