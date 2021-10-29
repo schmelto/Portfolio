b3d7f8048fcbc8b0a34d295687455a5f6b58c767
schmelto <30869493+schmelto@users.noreply.github.com>
2021-10-29 22:23:00 +0200

    fix: prettier issues

--- js/scripts.js
+++ js/scripts.js
@@ -94,19 +94,19 @@ var githubprojectsdomelement = document.getElementById('githubprojects');
 let githubprojects = [
   {
     name: 'schmelto/100-days-of-code',
-    icon: 'description'
+    icon: 'description',
   },
   {
     name: 'schmelto/NewsApp',
-    icon: 'article'
+    icon: 'article',
   },
   {
     name: 'schmelto/Portfolio',
-    icon: 'person'
+    icon: 'person',
   },
   {
     name: 'schmelto/ImpactHackathon',
-    icon: 'home'
+    icon: 'home',
   },
 ];
 
