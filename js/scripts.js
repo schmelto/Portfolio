b97a5352f2d52860c96042bc726303442458c5dc
Tom Schmelzer <30869493+schmelto@users.noreply.github.com>
2022-09-01 10:30:20 +0000

    feat: add sponsors button resolve #219

--- js/scripts.js
+++ js/scripts.js
@@ -134,16 +134,3 @@ function createprojectcard(project, icon) {
      </div>`;
   return projectcard;
 }
-
-function getVersionOfPortfolio() {
-  fetch(`https://api.github.com/repos/schmelto/Portfolio/releases/latest`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((release) => {
-      document.getElementById(
-        'version'
-      ).innerHTML = `- <a href="${release.html_url}" target="_blank" rel="noopener">${release.tag_name}</a>`;
-    });
-}
-getVersionOfPortfolio();
