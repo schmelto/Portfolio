92902f808f2c9ca0eb9b5547607cb7257a7dfa7b
schmelto <schmelto@users.noreply.github.com>
2023-11-25 18:39:08 +0000

    style: format files

--- js/version.js
+++ js/version.js
@@ -4,9 +4,8 @@ function getVersionOfPortfolio() {
       return response.json();
     })
     .then((release) => {
-      document.getElementById(
-        'version',
-      ).innerHTML = `- <a href="${release.html_url}" target="_blank" rel="noopener">${release.tag_name}</a>`;
+      document.getElementById('version').innerHTML =
+        `- <a href="${release.html_url}" target="_blank" rel="noopener">${release.tag_name}</a>`;
     });
 }
 getVersionOfPortfolio();
